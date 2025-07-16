bcf57603f62d07a07901f64f18a43d6f2c2aecf6
cascaruletas2 <cascaruletas2@gmail.com>
2025-07-16 04:47:54 +0100

    Add DOM, Modal and Form elements

--- src/Views/profile.js
+++ src/Views/profile.js
@@ -1,3 +1,19 @@
 import { getCurrentUser, logoutUser } from "../Auth/auth.js";
 import { getAllUsers, updateUser } from "../../API/userAPI.js";
 import { fetchProductImages } from "../../API/ApiProducts.js";
+
+// DOM elements
+const profileName = document.getElementById('profile-name');
+const profileLocation = document.getElementById('profile-location');
+const profileImage = document.getElementById('profile-image');
+const profileRating = document.getElementById('profile-rating');
+const profileContent = document.getElementById('profile-content');
+
+// Modal elements
+const editModal = document.getElementById('edit-modal');
+const addProductModal = document.getElementById('add-product-modal');
+const closeModalButtons = document.querySelectorAll('.close-modal');
+
+// Form elements
+const editProfileForm = document.getElementById('edit-profile-form');
+const addProductForm = document.getElementById('add-product-form');
