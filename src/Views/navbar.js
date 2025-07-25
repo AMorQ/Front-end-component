2702c2e2b8e3fb3dd12d59773f10be688eaa7bd9
cascaruletas2 <cascaruletas2@gmail.com>
2025-07-25 16:08:17 +0100

    Add navbar.js and init.js to separate it from main.js

---
+++ src/Views/navbar.js
@@ -0,0 +1,28 @@
+// src/navbar.js
+export function injectNavbar() {
+  const navbarHTML = `
+    <nav id="navbar">
+      <div class="navbar-container">
+        <div class="navbar-logo">
+          <img src="/src/assets/LOGO1.png" alt="Logo" />
+        </div>
+        <div class="navbar-links">
+          <a href="index.html">Home</a>
+          <a href="product.html">Products</a>
+          <a href="contact.html">Contact</a>
+          <a href="profile.html">Profile</a>
+          <a href="auth.html">Authentication</a>
+        </div>
+        <div class="navbar-cart">
+          <a href="#">
+            <img src="/src/assets/cart1.png" alt="Cart" />
+          </a>
+        </div>
+      </div>
+    </nav>
+  `;
+
+  const navbarContainer = document.createElement('div');
+  navbarContainer.innerHTML = navbarHTML;
+  document.body.insertBefore(navbarContainer, document.body.firstChild);
+}
