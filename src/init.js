9befd6ec70ecd4e1d6b653ea38f792b1ff8fba5f
NataliiaCodespace <ndziadevych@gmail.com>
2025-07-31 20:23:18 +0100

    [Add] shopping cart

--- src/init.js
+++ src/init.js
@@ -1,5 +1,6 @@
 import { injectFooter } from './utilsfooter.js';
 import { injectNavbar } from './navbar.js';
+import './Views/shoppingCart.js';
 
 document.addEventListener('DOMContentLoaded', () => {
   injectNavbar();
